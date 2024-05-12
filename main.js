f6f4f2b46d86a7b4b0b56042fd61bddbbf5b8f01
Fabricio Andrade <fabricio.solutions13@gmail.com>
2024-05-12 12:17:55 +0200

    fff

---
+++ main.js
@@ -0,0 +1,65 @@
+
+//Exercicio 01
+
+//alert("Hello World");
+
+//Exercicio 02
+
+//let numberOne = prompt("Diga o primeiro numero");
+//let numberTwo = prompt("Diga o segundo numero");
+//let result = Number(numberOne) + Number(numberTwo)
+//alert("Resultado final: " + result);
+
+//Exercicio 03 
+
+//let numberTest = prompt("Diga algo");
+
+//if (numberTest = Number(numberTest)) {
+  //alert("Isto e o numero")
+//} else {
+  //alert("Nao e um numero")
+//}
+
+//Exercicio 04
+
+//var isString2 = 2;
+
+//if (typeof isString2 == 'string') {
+ // alert('É uma string')
+//} else {
+ // alert('Não é uma string')
+//}
+
+//Exercicio 05
+
+//var isString2 = true;
+
+//if (typeof isString2 == 'boolean') {
+ // alert('É boolean'
+ // )
+//} else {
+ // alert('Não é uma boolean')
+//}
+
+//Exercio 06 07 08
+
+//var isOne = 10;
+//var isTwo = 5;
+
+//var result = Number(isOne) - Number(isTwo);
+//var result1 = Number(isOne) * Number(isTwo);
+//var result2 = Number(isOne) / Number(isTwo);
+
+//alert("The result is: " + result)
+//alert("The result is: " + result1)
+//alert("The result is: " + result2)
+
+//Exercio 09 10
+
+let isWhat = prompt("Diga um numero");
+
+if ((Number(isWhat) % 2) === 0) {
+  alert("numero e par")
+} else if ((Number(isWhat) % 2) != 0){
+  alert("numero e impar")
+}
